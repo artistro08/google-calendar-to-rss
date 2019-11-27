cf7610b7d4a0f91048c797077682f66831ce2b97
Devin Green <design@devingreen.me>
2019-11-27 09:44:54 -0600

    Fixing Branches
    This reverts commit 053c37637dcec839f960ce3f22fe91a0c858bf3f.

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,3 +1,19 @@
-@foreach ($events as $event)
-    {{ dd($event->summary) }}
-@endforeach
+<feed xmlns="http://www.w3.org/2005/Atom">
+
+    <title>CCN Featured RSS Feed</title>
+    <link href="https://ccnicr.com/" />
+    <author>
+        <name>Corridorn Connection Network</name>
+    </author>
+    <id>11245566856655</id>
+    @foreach ($events as $event)
+        <entry>
+            <title>{{ $event->summary }}</title>
+            <link href="{{ $event->url }}" />
+            <id>{{ $event->id }}</id>
+            <summary>{{ $event->startDate }} {{ $event->startDateTime }}</summary>
+            <content>{{ $event->description }}</content>
+        </entry>
+    @endforeach
+</feed>
+
