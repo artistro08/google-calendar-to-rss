37f4e45c4e082d048e8c9585e5cfc54ea2b507aa
Devin Green <design@devingreen.me>
2020-01-03 20:21:50 -0600

    Added date range for events

--- app/Http/Controllers/CalendarRSSController.php
+++ app/Http/Controllers/CalendarRSSController.php
@@ -5,13 +5,15 @@ namespace App\Http\Controllers;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Spatie\GoogleCalendar\Event;
-use Carbon;
+use Carbon\Carbon;
 
 class CalendarRSSController extends Controller
 {
     public function getIndex()
     {
-        $events = Event::get();
+        $sevenDaysAhead = Carbon::now()->addDays(7);
+        $events = Event::get(Carbon::now(), Carbon::now()->addDays(7));
+        dd($events);
         return response()->view('index', ['events' => $events])->header('Content-Type', 'text/xml');
     }
 }
