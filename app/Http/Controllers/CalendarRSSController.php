9fc73191141b40ed86a03a981bd6a3c051a28fc8
Devin Green <design@devingreen.me>
2020-02-21 19:37:54 -0600

    redesign of the code

--- app/Http/Controllers/CalendarRSSController.php
+++ app/Http/Controllers/CalendarRSSController.php
@@ -11,9 +11,11 @@ class CalendarRSSController extends Controller
 {
     public function getIndex()
     {
-        $startOfWeek = Carbon::now()->modify('this week');
-        $endOfWeek = Carbon::now()->modify('this week +6 days');
+        $startOfWeek = Carbon::now()->modify('tomorrow');
+        $endOfWeek = Carbon::now()->modify('today +30 days');
         $events = Event::get($startOfWeek, $endOfWeek);
-        return response()->view('index', ['events' => $events])->header('Content-Type', 'text/xml');
+        return response()
+            ->view('index', ['events' => $events])
+            ->header('Content-Type', 'application/xml');
     }
 }
